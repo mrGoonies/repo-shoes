987a82d4100ba19ee0cdc0b6836a02d4083f462e
mrGoonies <mrgoonies@mrGooniess-MacBook-Pro.local>
2022-10-27 01:17:07 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'repo-shoes',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
