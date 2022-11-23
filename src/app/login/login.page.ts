630d609469f91fa6ead61f1a344f04cd4b050c2e
mrGoonies <gooniesdev@gmail.com>
2022-11-23 11:59:21 -0300

    Agregando funcionalidad de routing

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +11,19 @@ export class LoginPage implements OnInit {
   imageLogo: string = 'https://img.freepik.com/premium-vector/illustration-pair-sneaker_289678-26.jpg?w=2000'
 
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+
+  navigateToHome = () => {
+    this.router.navigate(['/home'])
+  }
+
+  navigateToSignUp = () => {
+    this.router.navigate(['/sign-up'])
+  }
+
+
 
 }
