b9b6afd876a72819b9aa24c62121a1e3b4c0ff14
mrGoonies <gooniesdev@gmail.com>
2022-12-01 20:06:12 -0300

    Agregando primeras pruebas (conceptuales)

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,29 @@
 import { AppPage } from './app.po';
+// test Prueba
+import { browser, element, by } from 'protractor';
 
+describe('tabs', () => {
+  beforeEach(() => {
+    browser.get('/');
+  });
+
+  it('the Tab One tab is displayed by default', () => {
+    expect(element(by.css('.tab-selected ion-label')).getText()).toContain(
+      'Tab One'
+    );
+  });
+
+  it('the user can navigate to the Tab Two tab', async () => {
+    await element(by.css('[tab=tab2]')).click();
+
+    browser.driver.sleep(500);
+    expect(element(by.css('.tab-selected ion-label')).getText()).toContain(
+      'Tab Two'
+    );
+  });
+});
+
+// Test Default
 describe('new App', () => {
   let page: AppPage;
 
